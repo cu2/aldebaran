76c5755a38bbe45816080d7168169ceef102a75b
Andras Nemeth <cucu.szpem@gmail.com>
2019-04-06 20:07:54 +0200

    [operands] updated address modes and proper testing

---
+++ instructions/operands.py
@@ -0,0 +1,235 @@
+from collections import namedtuple
+
+from utils import utils
+from utils.tokenizer import UnknownTokenError
+
+
+Operand = namedtuple('Operand', ['oplen', 'optype', 'opreg', 'opvalue', 'opbase', 'opoffset'])
+
+
+class RegisterError(Exception):
+    pass
+
+
+class InvalidRegisterNameError(RegisterError):
+    pass
+
+
+class InvalidRegisterCodeError(RegisterError):
+    pass
+
+
+class InvalidTokenLengthError(Exception):
+    pass
+
+
+class InvalidOperandError(Exception):
+    pass
+
+
+class InsufficientOperandBufferError(Exception):
+    pass
+
+
+OPLEN_BYTE = 0
+OPLEN_WORD = 1
+
+
+OPTYPE_VALUE = 0
+# dd, dddd
+# e.g. constants, interrupt and I/O numbers, absolute addresses
+
+OPTYPE_ADDRESS = 1
+# ^dddd
+# e.g. relative addresses (labels) that must be absolutized (for jumps, calls)
+
+OPTYPE_REGISTER = 2
+# AX, AL
+# e.g. temporary variables
+
+OPTYPE_ABS_REF_REG = 3
+# [AX+dd]
+# e.g. params (BP+), local variables (BP-), structs with address in reg
+
+OPTYPE_REL_REF_WORD = 4
+# [dddd]
+# e.g. global variables
+
+OPTYPE_REL_REF_WORD_BYTE = 5
+# [dddd+dd]
+# e.g. structs (address = global variable)
+
+OPTYPE_REL_REF_WORD_REG = 6
+# [dddd+AX]
+# e.g. arrays (address = global variable)
+
+OPTYPE_EXTENDED = 7
+# the next byte specifies the optype
+# not used yet
+
+
+WORD_REGISTERS = ['AX', 'BX', 'CX', 'DX', 'BP', 'SP', 'SI', 'DI']
+BYTE_REGISTERS = ['AL', 'AH', 'BL', 'BH', 'CL', 'CH', 'DL', 'DH']
+
+
+def get_register_code_by_name(register_name):
+    '''Return register code by name'''
+    try:
+        return WORD_REGISTERS.index(register_name)
+    except ValueError:
+        try:
+            return len(WORD_REGISTERS) + BYTE_REGISTERS.index(register_name)
+        except ValueError:
+            raise InvalidRegisterNameError(register_name)
+
+
+def get_register_name_by_code(register_code):
+    '''Return register name by code'''
+    if register_code < 0 or register_code >= len(WORD_REGISTERS) + len(BYTE_REGISTERS):
+        raise InvalidRegisterCodeError(register_code)
+    if register_code < len(WORD_REGISTERS):
+        return WORD_REGISTERS[register_code]
+    return BYTE_REGISTERS[register_code - len(WORD_REGISTERS)]
+
+
+def get_opbyte(oplen, optype, opreg_name=None):
+    if opreg_name is None:
+        opreg = 0
+    else:
+        opreg = get_register_code_by_name(opreg_name)
+    return (oplen << 7) + (optype << 4) + opreg
+
+
+def get_operand_opcode(token):
+    '''Return opcode of operand'''
+    if token.type == 'WORD_LITERAL':
+        return [
+            get_opbyte(OPLEN_WORD, OPTYPE_VALUE)
+        ] + utils.word_to_bytes(token.value)
+    if token.type == 'BYTE_LITERAL':
+        return [
+            get_opbyte(OPLEN_BYTE, OPTYPE_VALUE)
+        ] + utils.byte_to_bytes(token.value)
+    if token.type == 'ADDRESS_WORD_LITERAL':
+        return [
+            get_opbyte(OPLEN_WORD, OPTYPE_ADDRESS)
+        ] + utils.word_to_bytes(token.value)
+    if token.type == 'WORD_REGISTER':
+        return [
+            get_opbyte(OPLEN_WORD, OPTYPE_REGISTER, token.value)
+        ]
+    if token.type == 'BYTE_REGISTER':
+        return [
+            get_opbyte(OPLEN_BYTE, OPTYPE_REGISTER, token.value)
+        ]
+    if token.type in {'ABS_REF_REG', 'REL_REF_WORD', 'REL_REF_WORD_BYTE', 'REL_REF_WORD_REG'}:
+        if token.value.length == 'B':
+            oplen = OPLEN_BYTE
+        elif token.value.length == 'W':
+            oplen = OPLEN_WORD
+        else:
+            raise InvalidTokenLengthError()
+        optype = {
+            'ABS_REF_REG': OPTYPE_ABS_REF_REG,
+            'REL_REF_WORD': OPTYPE_REL_REF_WORD,
+            'REL_REF_WORD_BYTE': OPTYPE_REL_REF_WORD_BYTE,
+            'REL_REF_WORD_REG': OPTYPE_REL_REF_WORD_REG,
+        }[token.type]
+        if token.type == 'ABS_REF_REG':
+            opreg = token.value.base
+        elif token.type == 'REL_REF_WORD_REG':
+            opreg = token.value.offset
+        else:
+            opreg = None
+        if token.type == 'ABS_REF_REG':
+            oprest = utils.byte_to_bytes(token.value.offset)
+        elif token.type == 'REL_REF_WORD':
+            oprest = utils.word_to_bytes(token.value.base)
+        elif token.type == 'REL_REF_WORD_BYTE':
+            oprest = utils.word_to_bytes(token.value.base) + utils.byte_to_bytes(token.value.offset)
+        elif token.type == 'REL_REF_WORD_REG':
+            oprest = utils.word_to_bytes(token.value.base)
+        return [
+            get_opbyte(oplen, optype, opreg)
+        ] + oprest
+    raise UnknownTokenError()
+
+
+def parse_operand_buffer(operand_buffer, operand_count):
+    '''Return opcode_length (including instruction) and list of operands as Operand(oplen, optype, opreg, oprest) tuples from operand_buffer'''
+    operands = []
+    operand_buffer_idx = 0
+    try:
+        while True:
+            if len(operands) >= operand_count:
+                break
+            opbyte = operand_buffer[operand_buffer_idx]
+            operand_buffer_idx += 1
+            oplen = opbyte >> 7
+            optype = (opbyte & 0x7F) >> 4
+            raw_opreg = opbyte & 0x0F
+            opreg = None
+            opvalue = None
+            opbase = None
+            opoffset = None
+            if optype == OPTYPE_VALUE:
+                if oplen == OPLEN_WORD:
+                    opvalue = utils.bytes_to_word(
+                        operand_buffer[operand_buffer_idx+0],
+                        operand_buffer[operand_buffer_idx+1],
+                    )
+                    operand_buffer_idx += 2
+                else:
+                    opvalue = operand_buffer[operand_buffer_idx+0]
+                    operand_buffer_idx += 1
+            elif optype == OPTYPE_ADDRESS:
+                if oplen == OPLEN_WORD:
+                    opvalue = utils.bytes_to_word(
+                        operand_buffer[operand_buffer_idx+0],
+                        operand_buffer[operand_buffer_idx+1],
+                    )
+                    operand_buffer_idx += 2
+                else:
+                    raise InvalidOperandError(operand_buffer, operand_buffer_idx)
+            elif optype == OPTYPE_REGISTER:
+                opreg = get_register_name_by_code(raw_opreg)
+                if oplen == OPLEN_WORD:
+                    if opreg not in WORD_REGISTERS:
+                        raise InvalidRegisterCodeError()
+                else:
+                    if opreg not in BYTE_REGISTERS:
+                        raise InvalidRegisterCodeError()
+            elif optype == OPTYPE_ABS_REF_REG:
+                opreg = get_register_name_by_code(raw_opreg)
+                if opreg not in WORD_REGISTERS:
+                    raise InvalidRegisterCodeError()
+                opoffset = operand_buffer[operand_buffer_idx+0]
+                operand_buffer_idx += 1
+            elif optype == OPTYPE_REL_REF_WORD:
+                opbase = utils.bytes_to_word(
+                    operand_buffer[operand_buffer_idx+0],
+                    operand_buffer[operand_buffer_idx+1],
+                )
+                operand_buffer_idx += 2
+            elif optype == OPTYPE_REL_REF_WORD_BYTE:
+                opbase = utils.bytes_to_word(
+                    operand_buffer[operand_buffer_idx+0],
+                    operand_buffer[operand_buffer_idx+1],
+                )
+                opoffset = operand_buffer[operand_buffer_idx+2]
+                operand_buffer_idx += 3
+            elif optype == OPTYPE_REL_REF_WORD_REG:
+                opreg = get_register_name_by_code(raw_opreg)
+                if opreg not in WORD_REGISTERS:
+                    raise InvalidRegisterCodeError()
+                opbase = utils.bytes_to_word(
+                    operand_buffer[operand_buffer_idx+0],
+                    operand_buffer[operand_buffer_idx+1],
+                )
+                operand_buffer_idx += 2
+            else:
+                raise InvalidOperandError(operand_buffer, operand_buffer_idx)
+            operands.append(Operand(oplen, optype, opreg, opvalue, opbase, opoffset))
+    except IndexError:
+        raise InsufficientOperandBufferError(operand_buffer)
+    return operands, 1 + operand_buffer_idx
---
+++ tests/test_operands.py
@@ -0,0 +1,280 @@
+import unittest
+
+from instructions.operands import get_register_code_by_name, get_register_name_by_code,\
+    InvalidRegisterNameError, InvalidRegisterCodeError,\
+    get_opbyte,\
+    get_operand_opcode, OPLEN_BYTE, OPLEN_WORD,\
+    OPTYPE_VALUE, OPTYPE_ADDRESS, OPTYPE_REGISTER, OPTYPE_ABS_REF_REG,\
+    OPTYPE_REL_REF_WORD, OPTYPE_REL_REF_WORD_BYTE, OPTYPE_REL_REF_WORD_REG,\
+    InvalidTokenLengthError,\
+    parse_operand_buffer, InvalidOperandError, InsufficientOperandBufferError
+from utils.tokenizer import Token, Reference, UnknownTokenError
+from utils.utils import WordOutOfRangeError, ByteOutOfRangeError
+
+
+class TestRegisters(unittest.TestCase):
+
+    def test_get_register_code_by_name(self):
+        self.assertEqual(get_register_code_by_name('AX'), 0)
+        self.assertEqual(get_register_code_by_name('AL'), 8)
+        self.assertEqual(get_register_code_by_name('AH'), 9)
+        with self.assertRaises(InvalidRegisterNameError):
+            get_register_code_by_name('XX')
+
+    def test_get_register_name_by_code(self):
+        self.assertEqual(get_register_name_by_code(0), 'AX')
+        self.assertEqual(get_register_name_by_code(8), 'AL')
+        self.assertEqual(get_register_name_by_code(9), 'AH')
+        with self.assertRaises(InvalidRegisterCodeError):
+            get_register_name_by_code(-1)
+        with self.assertRaises(InvalidRegisterCodeError):
+            get_register_name_by_code(16)
+
+
+class TestGetOpbyte(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(get_opbyte(OPLEN_BYTE, OPTYPE_VALUE), 0)
+        self.assertEqual(get_opbyte(OPLEN_WORD, OPTYPE_VALUE), 128)
+        self.assertEqual(get_opbyte(OPLEN_BYTE, OPTYPE_VALUE, 'AX'), 0)
+        self.assertEqual(get_opbyte(OPLEN_WORD, OPTYPE_VALUE, 'AX'), 128)
+        self.assertEqual(get_opbyte(OPLEN_BYTE, OPTYPE_VALUE, 'BX'), 1)
+        self.assertEqual(get_opbyte(OPLEN_WORD, OPTYPE_VALUE, 'BX'), 129)
+
+
+class TestGetOperandOpcode(unittest.TestCase):
+
+    def test_literal(self):
+        self.assertListEqual(get_operand_opcode(Token('WORD_LITERAL', 65535)), [
+            get_opbyte(OPLEN_WORD, OPTYPE_VALUE),
+            0xFF, 0xFF,
+        ])
+        with self.assertRaises(WordOutOfRangeError):
+            get_operand_opcode(Token('WORD_LITERAL', 65536))
+        self.assertListEqual(get_operand_opcode(Token('BYTE_LITERAL', 255)), [
+            get_opbyte(OPLEN_BYTE, OPTYPE_VALUE),
+            0xFF,
+        ])
+        with self.assertRaises(ByteOutOfRangeError):
+            get_operand_opcode(Token('BYTE_LITERAL', -1))
+        self.assertListEqual(get_operand_opcode(Token('ADDRESS_WORD_LITERAL', 65535)), [
+            get_opbyte(OPLEN_WORD, OPTYPE_ADDRESS),
+            0xFF, 0xFF,
+        ])
+
+    def test_register(self):
+        self.assertListEqual(get_operand_opcode(Token('WORD_REGISTER', 'AX')), [
+            get_opbyte(OPLEN_WORD, OPTYPE_REGISTER, 'AX'),
+        ])
+        self.assertListEqual(get_operand_opcode(Token('BYTE_REGISTER', 'AL')), [
+            get_opbyte(OPLEN_BYTE, OPTYPE_REGISTER, 'AL'),
+        ])
+        self.assertListEqual(get_operand_opcode(Token('BYTE_REGISTER', 'AH')), [
+            get_opbyte(OPLEN_BYTE, OPTYPE_REGISTER, 'AH'),
+        ])
+        with self.assertRaises(InvalidRegisterNameError):
+            get_operand_opcode(Token('WORD_REGISTER', 'XX'))
+
+    def test_abs_ref(self):
+        self.assertListEqual(get_operand_opcode(Token('ABS_REF_REG', Reference('BX', 0, 'B'))), [
+            get_opbyte(OPLEN_BYTE, OPTYPE_ABS_REF_REG, 'BX'),
+            0x00,
+        ])
+        self.assertListEqual(get_operand_opcode(Token('ABS_REF_REG', Reference('BX', 255, 'W'))), [
+            get_opbyte(OPLEN_WORD, OPTYPE_ABS_REF_REG, 'BX'),
+            0xFF,
+        ])
+        with self.assertRaises(InvalidRegisterNameError):
+            get_operand_opcode(Token('ABS_REF_REG', Reference('XX', 0, 'W')))
+        with self.assertRaises(ByteOutOfRangeError):
+            get_operand_opcode(Token('ABS_REF_REG', Reference('BX', 256, 'W')))
+        with self.assertRaises(InvalidTokenLengthError):
+            get_operand_opcode(Token('ABS_REF_REG', Reference('AX', 0, '?')))
+
+    def test_rel_ref(self):
+        self.assertListEqual(get_operand_opcode(Token('REL_REF_WORD', Reference(65535, 0, 'B'))), [
+            get_opbyte(OPLEN_BYTE, OPTYPE_REL_REF_WORD),
+            0xFF, 0xFF,
+        ])
+        with self.assertRaises(WordOutOfRangeError):
+            get_operand_opcode(Token('REL_REF_WORD', Reference(65536, 0, 'B')))
+        self.assertListEqual(get_operand_opcode(Token('REL_REF_WORD_BYTE', Reference(65535, 255, 'B'))), [
+            get_opbyte(OPLEN_BYTE, OPTYPE_REL_REF_WORD_BYTE),
+            0xFF, 0xFF, 0xFF,
+        ])
+        self.assertListEqual(get_operand_opcode(Token('REL_REF_WORD_REG', Reference(65535, 'BX', 'B'))), [
+            get_opbyte(OPLEN_BYTE, OPTYPE_REL_REF_WORD_REG, 'BX'),
+            0xFF, 0xFF,
+        ])
+        with self.assertRaises(InvalidRegisterNameError):
+            get_operand_opcode(Token('REL_REF_WORD_REG', Reference(65535, 'XX', 'B')))
+
+    def test_other(self):
+        with self.assertRaises(UnknownTokenError):
+            get_operand_opcode(Token('unknown', 0))
+
+
+class TestParseOperandBuffer(unittest.TestCase):
+
+    def test_value(self):
+        operands, opcode_length = parse_operand_buffer([
+            get_opbyte(OPLEN_WORD, OPTYPE_VALUE),
+            0xFF, 0xFF, 0xFF, 0xFF,
+        ], 1)
+        self.assertEqual(len(operands), 1)
+        self.assertEqual(operands[0].oplen, OPLEN_WORD)
+        self.assertEqual(operands[0].optype, OPTYPE_VALUE)
+        self.assertIsNone(operands[0].opreg)
+        self.assertEqual(operands[0].opvalue, 65535)
+        self.assertIsNone(operands[0].opbase)
+        self.assertIsNone(operands[0].opoffset)
+        self.assertEqual(opcode_length, 4)
+        operands, opcode_length = parse_operand_buffer([
+            get_opbyte(OPLEN_BYTE, OPTYPE_VALUE),
+            0xFF, 0xFF, 0xFF, 0xFF,
+        ], 1)
+        self.assertEqual(len(operands), 1)
+        self.assertEqual(operands[0].oplen, OPLEN_BYTE)
+        self.assertEqual(operands[0].optype, OPTYPE_VALUE)
+        self.assertIsNone(operands[0].opreg)
+        self.assertEqual(operands[0].opvalue, 255)
+        self.assertIsNone(operands[0].opbase)
+        self.assertIsNone(operands[0].opoffset)
+        self.assertEqual(opcode_length, 3)
+
+    def test_address(self):
+        operands, opcode_length = parse_operand_buffer([
+            get_opbyte(OPLEN_WORD, OPTYPE_ADDRESS),
+            0xFF, 0xFF, 0xFF, 0xFF,
+        ], 1)
+        self.assertEqual(len(operands), 1)
+        self.assertEqual(operands[0].oplen, OPLEN_WORD)
+        self.assertEqual(operands[0].optype, OPTYPE_ADDRESS)
+        self.assertIsNone(operands[0].opreg)
+        self.assertEqual(operands[0].opvalue, 65535)
+        self.assertIsNone(operands[0].opbase)
+        self.assertIsNone(operands[0].opoffset)
+        self.assertEqual(opcode_length, 4)
+        with self.assertRaises(InvalidOperandError):
+            parse_operand_buffer([
+                get_opbyte(OPLEN_BYTE, OPTYPE_ADDRESS),
+                0xFF, 0xFF, 0xFF, 0xFF,
+            ], 1)
+
+    def test_register(self):
+        operands, opcode_length = parse_operand_buffer([
+            get_opbyte(OPLEN_WORD, OPTYPE_REGISTER, 'BX'),
+            0xFF, 0xFF, 0xFF, 0xFF,
+        ], 1)
+        self.assertEqual(len(operands), 1)
+        self.assertEqual(operands[0].oplen, OPLEN_WORD)
+        self.assertEqual(operands[0].optype, OPTYPE_REGISTER)
+        self.assertEqual(operands[0].opreg, 'BX')
+        self.assertIsNone(operands[0].opvalue)
+        self.assertIsNone(operands[0].opbase)
+        self.assertIsNone(operands[0].opoffset)
+        self.assertEqual(opcode_length, 2)
+        operands, opcode_length = parse_operand_buffer([
+            get_opbyte(OPLEN_BYTE, OPTYPE_REGISTER, 'AH'),
+            0xFF, 0xFF, 0xFF, 0xFF,
+        ], 1)
+        self.assertEqual(len(operands), 1)
+        self.assertEqual(operands[0].oplen, OPLEN_BYTE)
+        self.assertEqual(operands[0].optype, OPTYPE_REGISTER)
+        self.assertEqual(operands[0].opreg, 'AH')
+        self.assertIsNone(operands[0].opvalue)
+        self.assertIsNone(operands[0].opbase)
+        self.assertIsNone(operands[0].opoffset)
+        self.assertEqual(opcode_length, 2)
+        with self.assertRaises(InvalidRegisterCodeError):
+            parse_operand_buffer([
+                get_opbyte(OPLEN_BYTE, OPTYPE_REGISTER, 'BX'),
+                0xFF, 0xFF, 0xFF, 0xFF,
+            ], 1)
+
+    def test_abs_ref(self):
+        operands, opcode_length = parse_operand_buffer([
+            get_opbyte(OPLEN_WORD, OPTYPE_ABS_REF_REG, 'BX'),
+            0xFF, 0xFF, 0xFF, 0xFF,
+        ], 1)
+        self.assertEqual(len(operands), 1)
+        self.assertEqual(operands[0].oplen, OPLEN_WORD)
+        self.assertEqual(operands[0].optype, OPTYPE_ABS_REF_REG)
+        self.assertEqual(operands[0].opreg, 'BX')
+        self.assertIsNone(operands[0].opvalue)
+        self.assertIsNone(operands[0].opbase)
+        self.assertEqual(operands[0].opoffset, 255)
+        self.assertEqual(opcode_length, 3)
+        with self.assertRaises(InvalidRegisterCodeError):
+            parse_operand_buffer([
+                get_opbyte(OPLEN_WORD, OPTYPE_ABS_REF_REG, 'AH'),
+                0xFF, 0xFF, 0xFF, 0xFF,
+            ], 1)
+
+    def test_rel_ref(self):
+        operands, opcode_length = parse_operand_buffer([
+            get_opbyte(OPLEN_WORD, OPTYPE_REL_REF_WORD),
+            0xFF, 0xFF, 0xFF, 0xFF,
+        ], 1)
+        self.assertEqual(len(operands), 1)
+        self.assertEqual(operands[0].oplen, OPLEN_WORD)
+        self.assertEqual(operands[0].optype, OPTYPE_REL_REF_WORD)
+        self.assertIsNone(operands[0].opreg)
+        self.assertIsNone(operands[0].opvalue)
+        self.assertEqual(operands[0].opbase, 65535)
+        self.assertIsNone(operands[0].opoffset)
+        self.assertEqual(opcode_length, 4)
+
+        operands, opcode_length = parse_operand_buffer([
+            get_opbyte(OPLEN_WORD, OPTYPE_REL_REF_WORD_BYTE),
+            0xFF, 0xFF, 0xFF, 0xFF,
+        ], 1)
+        self.assertEqual(len(operands), 1)
+        self.assertEqual(operands[0].oplen, OPLEN_WORD)
+        self.assertEqual(operands[0].optype, OPTYPE_REL_REF_WORD_BYTE)
+        self.assertIsNone(operands[0].opreg)
+        self.assertIsNone(operands[0].opvalue)
+        self.assertEqual(operands[0].opbase, 65535)
+        self.assertEqual(operands[0].opoffset, 255)
+        self.assertEqual(opcode_length, 5)
+
+        operands, opcode_length = parse_operand_buffer([
+            get_opbyte(OPLEN_WORD, OPTYPE_REL_REF_WORD_REG, 'BX'),
+            0xFF, 0xFF, 0xFF, 0xFF,
+        ], 1)
+        self.assertEqual(len(operands), 1)
+        self.assertEqual(operands[0].oplen, OPLEN_WORD)
+        self.assertEqual(operands[0].optype, OPTYPE_REL_REF_WORD_REG)
+        self.assertEqual(operands[0].opreg, 'BX')
+        self.assertIsNone(operands[0].opvalue)
+        self.assertEqual(operands[0].opbase, 65535)
+        self.assertIsNone(operands[0].opoffset)
+        self.assertEqual(opcode_length, 4)
+
+    def test_multiple_operands(self):
+        operands, opcode_length = parse_operand_buffer([
+            get_opbyte(OPLEN_WORD, OPTYPE_REGISTER, 'BX'),
+            get_opbyte(OPLEN_WORD, OPTYPE_VALUE),
+            0xFF, 0xFF, 0xFF, 0xFF,
+        ], 2)
+        self.assertEqual(len(operands), 2)
+        self.assertEqual(operands[0].oplen, OPLEN_WORD)
+        self.assertEqual(operands[0].optype, OPTYPE_REGISTER)
+        self.assertEqual(operands[0].opreg, 'BX')
+        self.assertIsNone(operands[0].opvalue)
+        self.assertIsNone(operands[0].opbase)
+        self.assertIsNone(operands[0].opoffset)
+        self.assertEqual(operands[1].oplen, OPLEN_WORD)
+        self.assertEqual(operands[1].optype, OPTYPE_VALUE)
+        self.assertIsNone(operands[1].opreg)
+        self.assertEqual(operands[1].opvalue, 65535)
+        self.assertIsNone(operands[1].opbase)
+        self.assertIsNone(operands[1].opoffset)
+        self.assertEqual(opcode_length, 5)
+
+    def test_not_enough_buffer(self):
+        with self.assertRaises(InsufficientOperandBufferError):
+            parse_operand_buffer([
+                get_opbyte(OPLEN_WORD, OPTYPE_VALUE),
+                0xFF,
+            ], 1)
