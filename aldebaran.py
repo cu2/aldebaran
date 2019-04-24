bcf4d61b53055d78676eb46868b3251e75aca8f9
Andras Nemeth <cucu.szpem@gmail.com>
2019-04-24 23:03:24 +0200

    [aldebaran] refactored and documented

--- aldebaran.py
+++ aldebaran.py
@@ -1,31 +1,69 @@
+'''
+Load executable file and run it
+
+Usage: python aldebaran.py <file>
+'''
+
 import argparse
-import sys
 import time
-import traceback
 
-import assembler
 from instructions.instruction_set import INSTRUCTION_SET
-
-from utils import config, errors, utils
-from utils.executable import Executable
-
 from hardware import device_controller
 from hardware import interrupt_controller
 from hardware import timer
-
 from hardware.clock import Clock
 from hardware.cpu import CPU
 from hardware.ram import RAM
+from utils import boot
+from utils import config
+from utils import utils
+from utils import executable
+
+
+def main():
+    '''
+    Entry point of script
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'file',
+        help='Aldebaran executable file'
+    )
+    # TODO: add verbosity
+    args = parser.parse_args()
+    boot_file = args.file
+    loggers = config.loggers
+    ioports = [
+        device_controller.IOPort(ioport_number, loggers['device_controller'])
+        for ioport_number in range(config.number_of_ioports)
+    ]
+    aldebaran = Aldebaran({
+        'clock': Clock(config.clock_freq, loggers['clock']),
+        'cpu': CPU(config.system_addresses, config.system_interrupts, INSTRUCTION_SET, loggers['user'], loggers['cpu']),
+        'ram': RAM(config.ram_size, loggers['ram']),
+        'interrupt_controller': interrupt_controller.InterruptController(loggers['interrupt_controller']),
+        'device_controller': device_controller.DeviceController(
+            config.aldebaran_host, config.aldebaran_base_port + config.device_controller_port,
+            config.system_addresses, config.system_interrupts,
+            ioports,
+            loggers['device_controller'],
+        ),
+        'timer': timer.Timer(config.timer_freq, loggers['timer']),
+    }, loggers['aldebaran'])
+    aldebaran.boot(boot_file)
+    aldebaran.run()
 
 
 class Aldebaran(utils.Hardware):
+    '''
+    Aldebaran
+    '''
 
     def __init__(self, components, log=None):
         utils.Hardware.__init__(self, log)
         self.clock = components['clock']
         self.cpu = components['cpu']
         self.ram = components['ram']
-        self.bios = components['bios']
         self.interrupt_controller = components['interrupt_controller']
         self.device_controller = components['device_controller']
         self.timer = components['timer']
@@ -35,92 +73,57 @@ class Aldebaran(utils.Hardware):
         self.device_controller.register_architecture(self.interrupt_controller, self.ram)
         self.timer.register_architecture(self.interrupt_controller)
 
-    def load_bios(self):
-        self.log.log('aldebaran', 'Loading BIOS...')
-        for key, (start_pos, contents) in self.bios.contents.items():
-            for rel_pos, content in enumerate(contents):
-                pos = start_pos + rel_pos
-                self.ram.write_byte(pos, content)
-        self.log.log('aldebaran', 'BIOS loaded.')
-        return 0
+    def boot(self, boot_file):
+        '''
+        Load boot image and boot file
+        '''
+        self.log.log('aldebaran', 'Loading boot file {}...'.format(boot_file))
+        boot_loader = boot.BootLoader(self.ram)
+        boot_image = config.create_boot_image()  # later it should be loaded from file
+        boot_loader.load_image(0, boot_image)
+        boot_exe = executable.Executable()
+        boot_exe.load_from_file(boot_file)
+        if boot_exe.version != 1:
+            raise UnsupportedExecutableVersionError('Unsupported version: {}'.format(boot_exe.version))
+        boot_loader.load_executable(config.system_addresses['entry_point'], boot_exe)
+        self.log.log('aldebaran', 'Loaded.')
 
     def run(self):
+        '''
+        Start device controller, timer and clock
+        '''
         self.log.log('aldebaran', 'Started.')
-        retval = self.load_bios()
-        if retval != 0:
-            return retval
         retval = self.device_controller.start()
         if retval != 0:
-            return retval
+            raise AldebaranError('Could not start Device Controller')
         retval = self.timer.start()
         if retval != 0:
-            return retval
+            raise AldebaranError('Could not start Timer')
         start_time = time.time()
         try:
             retval = self.clock.run()
-        except Exception:
-            tb = traceback.format_exc()
-            self.log.log('aldebaran', 'EXCEPTION: %s' % tb)
-            retval = 1
-        stop_time = time.time()
-        self.timer.stop()
-        self.device_controller.stop()
-        self.log.log('aldebaran', 'Stopped after %s steps in %s sec (%s Hz).' % (
-            self.clock.step_count,
-            round(stop_time - start_time, 2),
-            int(round(self.clock.step_count / (stop_time - start_time)))
-        ))
-        return retval
+            if retval != 0:
+                raise AldebaranError('Could not start Clock')
+        finally:
+            stop_time = time.time()
+            self.timer.stop()
+            self.device_controller.stop()
+            self.log.log('aldebaran', 'Stopped after %s steps in %s sec (%s Hz).' % (
+                self.clock.step_count,
+                round(stop_time - start_time, 2),
+                round(self.clock.step_count / (stop_time - start_time)),
+            ))
 
 
-class BIOS(utils.Hardware):
+# pylint: disable=missing-docstring
 
-    def __init__(self, contents):
-        utils.Hardware.__init__(self)
-        self.contents = contents
+class AldebaranError(Exception):
+    pass
+
+
+class UnsupportedExecutableVersionError(AldebaranError):
+    pass
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'file',
-        help='ALDB binary file'
-    )
-    # TODO: add verbosity
-    args = parser.parse_args()
-    # logging
-    loggers = config.loggers
-    # bios
-    exe = Executable()
-    exe.load_from_file(args.file)
-    if exe.version != 1:
-        print('Unsupported version: {}'.format(exe.version))
-        sys.exit(1)
-    start_program = exe.opcode
-    instruction_mapping = {
-        inst.__name__: (opcode, inst)
-        for opcode, inst in INSTRUCTION_SET
-    }
-    bios = BIOS({
-        'start': (config.system_addresses['entry_point'], start_program),
-        'default_interrupt_handler': (config.system_addresses['default_interrupt_handler'], [instruction_mapping['IRET'][0]]),
-        'IVT': (config.system_addresses['IVT'], utils.word_to_binary(config.system_addresses['default_interrupt_handler']) * config.number_of_interrupts),
-    })
-    # start
-    aldebaran = Aldebaran({
-        'clock': Clock(config.clock_freq, loggers['clock']),
-        'cpu': CPU(config.system_addresses, config.system_interrupts, INSTRUCTION_SET, loggers['user'], loggers['cpu']),
-        'ram': RAM(config.ram_size, loggers['ram']),
-        'bios': bios,
-        'interrupt_controller': interrupt_controller.InterruptController(loggers['interrupt_controller']),
-        'device_controller': device_controller.DeviceController(
-            config.aldebaran_host, config.aldebaran_base_port + config.device_controller_port,
-            config.system_addresses, config.system_interrupts,
-            [device_controller.IOPort(ioport_number, loggers['device_controller']) for ioport_number in range(config.number_of_ioports)],
-            loggers['device_controller'],
-        ),
-        'timer': timer.Timer(config.timer_freq, loggers['timer']),
-    }, loggers['aldebaran'])
-    retval = aldebaran.run()
-    print()
-    sys.exit(retval)
+    main()
