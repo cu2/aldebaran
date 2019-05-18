87d9c6198ebce5240dffc30f3ce28b65061dc03e
Andras Nemeth <cucu.szpem@gmail.com>
2019-05-19 00:36:03 +0200

    [instructions] ENTER updated, LVRET instead of LEAVE and RETPOP

--- instructions/instruction_set/control_flow.py
+++ instructions/instruction_set/control_flow.py
@@ -162,27 +162,6 @@ class CALL(Instruction):
         return self.get_operand(0)
 
 
-class ENTER(Instruction):
-    '''Enter subroutine: set frame pointer and allocate <op0> bytes on stack for local variables'''
-    # TODO: upgrade
-
-    operand_count = 1
-
-    def do(self):
-        self.cpu.stack.push_word(self.cpu.registers.get_register('BP'))
-        self.cpu.registers.set_register('BP', self.cpu.registers.get_register('SP'))
-        self.cpu.registers.set_register('SP', self.cpu.registers.get_register('SP') - self.get_operand(0))
-
-
-class LEAVE(Instruction):
-    '''Leave subroutine: free stack allocated for local variables'''
-    # TODO: upgrade
-
-    def do(self):
-        self.cpu.registers.set_register('SP', self.cpu.registers.get_register('BP'))
-        self.cpu.registers.set_register('BP', self.cpu.stack.pop_word())
-
-
 class RET(Instruction):
     '''Return from subroutine'''
 
@@ -190,15 +169,35 @@ class RET(Instruction):
         return self.cpu.stack.pop_word()
 
 
-class RETPOP(Instruction):
-    '''Return from subroutine and pop <op0> bytes'''
-    # TODO: upgrade
+class ENTER(Instruction):
+    '''
+    Enter subroutine: set frame pointer and allocate <op1> bytes on stack for local variables
 
-    operand_count = 1
+    - <op0> = byte count of parameters (allocated by PUSH instructions in caller)
+    - <op1> = byte count of local variables (allocated by ENTER instruction in callee)
+    '''
+
+    operand_count = 2
+    oplens = ['BB']
 
     def do(self):
+        self.cpu.stack.push_byte(self.get_operand(0))
+        self.cpu.stack.push_byte(self.get_operand(1))
+        self.cpu.stack.push_word(self.cpu.registers.get_register('BP'))
+        self.cpu.registers.set_register('BP', self.cpu.registers.get_register('SP'))
+        self.cpu.registers.set_register('SP', self.cpu.registers.get_register('SP') - self.get_operand(1))
+
+
+class LVRET(Instruction):
+    '''Leave subroutine and return from it: free stack allocated for local variables and parameters'''
+
+    def do(self):
+        self.cpu.registers.set_register('SP', self.cpu.registers.get_register('BP'))
+        self.cpu.registers.set_register('BP', self.cpu.stack.pop_word())
+        self.cpu.stack.pop_byte()  # byte count of local variables
+        byte_count_of_parameters = self.cpu.stack.pop_byte()
         next_ip = self.cpu.stack.pop_word()
-        self.cpu.registers.set_register('SP', self.cpu.registers.get_register('SP') + self.get_operand(0))
+        self.cpu.registers.set_register('SP', self.cpu.registers.get_register('SP') + byte_count_of_parameters)
         return next_ip
 
 
